02ac2608c250d81ada861c39d0e8d68a6d0edfa4
Ileass Bouhout <ileass.bouhout@gmail.com>
2025-02-01 20:07:14 -0500

    ma branch first commit

--- wildfire_project/wildfire_project/settings.py
+++ wildfire_project/wildfire_project/settings.py
@@ -12,12 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-u)=ouwu4*qc-pkhw=69esqqm38s(vc^%%r&o)0*vncusn&w$v^'
