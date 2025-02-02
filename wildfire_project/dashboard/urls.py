3d0e48fdeaded0a4ca3ac063de46ddf802dda020
Loky115 <lokeshwarbasdeo23@gmail.com>
2025-02-01 19:37:23 -0500

    firebase connecter

--- wildfire_project/dashboard/urls.py
+++ wildfire_project/dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Main dashboard view
+    path('test-firebase/', views.test_firebase, name='test_firebase'),
 ]
--- wildfire_project/dashboard/views.py
+++ wildfire_project/dashboard/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pyrebase
+
+config = {
+    "apiKey":"AIzaSyBds96dPAAEGuzNKzfZDoIvZacCjmPN67U",
+    "authDomain": "wildfire-8b733.firebaseapp.com",
+    "databaseURL": "https://wildfire-8b733-default-rtdb.firebaseio.com",
+    "projectId": "wildfire-8b733",
+    "storageBucket": "wildfire-8b733.firebasestorage.app",
+    "messagingSenderId":  "1079881094805",
+    "appId": "1:1079881094805:web:5faa84ca6a6ec506c02d9c",
+}
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
 
 def index(request):
     return render(request, 'dashboard/index.html')
 
 
+def test_firebase(request):
+    # Write data to Firebase
+    data = {"name": "Test User", "email": "testuser@example.com"}
+    database.child("users").push(data)
 
+    # Read data from Firebase
+    users = database.child("users").get().val()
+
+    return HttpResponse(f"Users in database: {users}")
